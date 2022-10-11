98d68b900783aafaf9c4f3afbea4a338daf5fe10
KatKlee <katklee2022@gmail.com>
2022-10-11 18:17:39 +0200

    updated server

--- controller/seatcontroller.js
+++ controller/seatcontroller.js
@@ -26,6 +26,9 @@ export const changeSeatState = (req, res) => {
         .then(data => JSON.parse(data))
         .then(obj => {
             obj.replace(seat) // geben wir hier an dass der state von true zu false bzw. immer ins gegenteil geändert wird?
+            // erst find (id) und dann können wir den state des entsprechenden seats ändern
+            //object.state=false (anstatt replace)
+
             //write into file
             // .stringify() method converts the JavaScript value to a JSON string
             writeIntoFile('data.json', JSON.stringify(obj))
